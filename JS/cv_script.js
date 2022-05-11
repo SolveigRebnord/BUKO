0b19eb67e5a242ce2ca9d2f1d86f21395e4baa88
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2022-05-11 21:05:10 +0200

    .

---
+++ JS/cv_script.js
@@ -0,0 +1,50 @@
+console.log("hello from cv side, skal jeg heller bare lage dette i overlay?")
+
+
+
+const queryString = window.location.search;
+const id = new URLSearchParams(queryString).get('id');
+//console.log(id);
+if (!id) { window.location = "members.html"; }
+
+
+
+
+const showCV = document.querySelector(".showCV")
+
+
+fetch(`https://www.sunroad.no/exam/wp-json/wc/store/products/${id}?_embed`) 
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    listSinglePost(data);
+})
+.catch(error => error.innerHTML = "Something's wrong!");
+
+
+
+listSinglePost = (data) => {
+
+    let title = data.name;
+    document.title = "CV | " + title;
+
+    let priceBefore = data.prices.price;
+    let intoDate = priceBefore.slice(0,2) + "." + priceBefore.slice(2,4) + "." + priceBefore.slice(4,8);
+    console.log(intoDate);
+  
+    let content = 
+    `<div class="cvdiv">
+    <h2>${data.name}</h2>
+    <p>${intoDate}</p>
+    <p>${data.categories[0].name}</p>
+    
+    ${data.description}
+    <img class="memberimg" src="${data.images[0].src}" alt=${data.images[0].alt}>
+    </div>`
+
+    showCV.innerHTML = content;
+  
+
+    
+}
--- JS/members_script.js
+++ JS/members_script.js
@@ -5,7 +5,14 @@ fetch ("https://www.sunroad.no/exam/wp-json/wc/store/products?_embed")
 .then(response => response.json())
 .then(data => {
     console.log(data);
+
+    for (let object of data) {
+        
+    if (object.tags[0].id = "21") {
+        console.log(object.tags)
     listPost(data);
+    }
+    }
 })
 .catch((error) => {
     console.error('Error:', error);
@@ -20,9 +27,11 @@ const outPut = document.querySelector(".showMembers");
 
     for (let post of array) {
 
-  list += `<h2>${post.name}</h2>
-  ${post.description}`
-
+  list += `<a href="cv.html?id=${post.id}"><div class="cvdiv">
+  <h2>${post.name}</h2>
+  <p class="instrument">${post.categories[0].name}</p>
+  <img class="memberimg" src="${post.images[0].src}" alt=${post.images[0].alt}>
+  </div></a>`
 
 }
 
