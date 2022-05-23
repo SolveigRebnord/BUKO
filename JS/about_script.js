516d8d1d2af6725344ebaa8b6be4f6b8fbf0c9e5
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2022-05-23 12:44:45 +0200

    .

--- JS/about_script.js
+++ JS/about_script.js
@@ -15,31 +15,48 @@ burgerMenu.addEventListener("click", function() {
     }
 });
 
+//-------------------------------------------------------------
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
-  
+  var mediaQuery = window.matchMedia("(max-width: 750px)");
 
+
+function myFunction(m) {
+  if (m.matches) { // If media query matches
+
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector("header > div > a > img").style.height = "50px";
+        document.querySelector("header").style.padding = "1rem 2rem";
+      
+
+      } 
     
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector("header > div > a > img").style.height = "30px";
-    document.querySelector("header").style.padding = "1rem 2rem 1rem 2rem";
-    document.querySelector("header").style.width = "96%";
-
-  } else {
-    document.querySelector("header > div > a > img").style.height = "80px";
-    document.querySelector("header").style.padding = "2rem";
-    document.querySelector("header").style.width = "96%";
   }
 
-  
+  else {
 
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.querySelector("header > div > a > img").style.height = "30px";
+      document.querySelector("header").style.padding = "1rem 2rem 1rem 2rem";
+    
 
-
+    } 
+    else {
+      document.querySelector("header > div > a > img").style.height = "80px";
+      document.querySelector("header").style.padding = "2rem";
+    
+    }
+  }
 }
 
+mediaQuery.addEventListener("change", myFunction) // Attach listener function on state changes
+myFunction(mediaQuery) // Call listener function at run time
+
+}
+//-------------------------------------------------------------------
 
 const story = document.querySelector("section > section.ourstory > h2");
 const conductor = document.querySelector("section > section.conductor > h2");
