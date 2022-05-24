aa7d16bc98241a17754fd5d3a68ed2b65bf95653
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2022-05-24 16:34:44 +0200

    .

--- JS/portfolio_script.js
+++ JS/portfolio_script.js
@@ -45,11 +45,10 @@ const section = document.querySelector(".sectionPortfolio");
             let intoDate = "0" + priceBefore.slice(0,1) + "." + priceBefore.slice(1,3) + "." + priceBefore.slice(3,7);
             //console.log(intoDate);
 
-            let backgroundIMG = post.images[0].src;
-              console.log(backgroundIMG)
 
             list += `<a href="event.html?id=${post.id}">
-            <div class="showPortfolio" style="background-image: url(${backgroundIMG})" >
+            <div class="showPortfolio">
+            <img src="${post.images[0].src}">
             <h2>${post.name}</h2>
             <p class="typeofevent">${post.categories[0].name}</p>
             <p class="date">${intoDate}</p>
@@ -63,24 +62,25 @@ const section = document.querySelector(".sectionPortfolio");
                 let intoDate = priceBefore.slice(0,2) + "." + priceBefore.slice(2,4) + "." + priceBefore.slice(4,8);
                 //console.log(intoDate);
            
-              let backgroundIMG = post.images[0].src;
-              console.log(backgroundIMG)
-
-            list += `<a href="event.html?id=${post.id}">
-            <div class="showPortfolio" style="background-image: url(${backgroundIMG})" >
-            <h2>${post.name}</h2>
-            <p class="typeofevent">${post.categories[0].name}</p>
-            <p class="date">${intoDate}</p>
-            ${post.short_description}</div></a>`
-
-         
+                list += `<a href="event.html?id=${post.id}">
+                <div class="showPortfolio">
+                <img src="${post.images[0].src}">
+                <h2>${post.name}</h2>
+                <p class="typeofevent">${post.categories[0].name}</p>
+                <p class="date">${intoDate}</p>
+                ${post.short_description}
+                </div></a>`
         }
-
-
-        
     }
   
     section.innerHTML = list;
   }
 
 
+
+const portfolioCheck = document.querySelector(".wholePortfolio > div > ul:first-of-type > li:first-of-type > input");
+console.log(portfolioCheck);
+
+if (portfolioCheck.checked = true) {
+  console.log("yey");
+}
