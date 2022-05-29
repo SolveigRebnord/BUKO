5250cb20cb1a07dcf538ddc223ee09d0b348a88c
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2022-05-29 21:51:17 +0200

    .

--- JS/cv_script.js
+++ JS/cv_script.js
@@ -37,13 +37,38 @@ listSinglePost = (data) => {
             console.log(priceBefore);
     let priceLength = priceBefore.length;
 
-            
+    let intoDate = "";
+   
     if (priceLength < 9) {
             
-    let intoDate = "0" + priceBefore.slice(0,1) + "." + priceBefore.slice(1,3) + "." + priceBefore.slice(3,7);
-            console.log(intoDate);
+     intoDate = "0" + priceBefore.slice(0,1) + "." + priceBefore.slice(1,3) + "." + priceBefore.slice(3,7);
+            //console.log(intoDate);
+    }
 
-            let content = 
+    else {
+         intoDate = priceBefore.slice(0,2) + "." + priceBefore.slice(2,4) + "." + priceBefore.slice(4,8);
+        //console.log(intoDate);
+    }
+
+    let city = "";
+
+    if (data.sku.includes("Oslo")) {
+        data.sku = "Oslo";
+    }
+
+    if (data.sku.includes("Bergen" || "bergen")) {
+        data.sku = "Bergen";
+    }
+
+    if (data.sku.includes("Bergen/Oslo")) {
+        data.sku = "Bergen/Oslo";
+    }
+
+
+    
+
+    
+    let content = 
 
             `
         <h1>${data.name}</h1>
@@ -62,31 +87,10 @@ listSinglePost = (data) => {
     
 
             showCV.innerHTML = content;
-    }
- 
-    else {
-        let intoDate = priceBefore.slice(0,2) + "." + priceBefore.slice(2,4) + "." + priceBefore.slice(4,8);
-        console.log(intoDate);
-        let content = 
-        `
-        <h1>${data.name}</h1>
-        <div class="cvDetails">
-            <div class="leftFlex">
-                <p>- ${data.categories[0].name}</p>
-                <img  onclick="openImg(this)"  class="memberimgCV" src="${data.images[0].src}" alt="${data.images[0].alt}">
-            </div>
-            <div class="rightFlex">
-                <p>${data.sku}</p>
-                <p>${intoDate}</p>
-                ${data.description}
-            </div>
-        </div>
-        <div>${data.short_description}</div>`
-
-        showCV.innerHTML = content;
-    }
-
 }
+  
+
+
 
 
  
